692b809b79475bcfba0513df221adb05fa6c9c8e
Jonathan <jtan13122004@gmail.com>
2025-04-03 23:50:07 +0800

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <>
+      <Toaster />
+      <h1 className="text-blue-500">Hello world</h1>;
+    </>
+  );
+}
+
+export default App;
