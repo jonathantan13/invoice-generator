cf8ec1a08c227dd398bc6914fe24f82609310694
Jonathan <jtan13122004@gmail.com>
2025-04-20 22:51:08 +0800

    Setting up google provider w/ next auth

---
+++ frontend/src/auth.ts
@@ -0,0 +1,18 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const GITHUB_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GITHUB_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
+if (!GITHUB_CLIENT_ID || !GITHUB_CLIENT_SECRET) {
+  throw new Error("Missing Google OAuth Credentials");
+}
+
+export const { handler, auth, signOut, signIn } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: GITHUB_CLIENT_ID,
+      clientSecret: GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,13 @@
+import { signIn } from "@/auth";
+import Link from "next/link";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link href="/">Home</Link>
+      <Link href="/create-invoice">Create invoice</Link>
+      <button onClick={signIn}>Sign in</button>
+      {/* <Link href={`invoice/${id}`}>Your invoices</Link> */}
+    </header>
+  );
 }
